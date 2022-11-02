50f3aa1c4338bf71a65ce0c009ddfa51897c1c2a
hanwenzhi <702054083@qq.com>
2022-11-02 15:40:32 +0800

    1

--- podofo-0.9.8/thirdparty/idn-1.22/include/idna_export.h
+++ podofo-0.9.8/thirdparty/idn-1.22/include/idna_export.h
@@ -1,15 +1,13 @@
 #pragma once
 
 
-#   define IDNAPI
-
-//#  if defined LIBIDN_BUILDING && defined HAVE_VISIBILITY && HAVE_VISIBILITY
-//#   define IDNAPI __attribute__((__visibility__("default")))
-//#  elif defined LIBIDN_BUILDING && defined _MSC_VER && ! defined LIBIDN_STATIC
-//#   define IDNAPI __declspec(dllexport)
-//#  elif defined _MSC_VER && ! defined LIBIDN_STATIC
-//#   define IDNAPI __declspec(dllimport)
-//#  else
-//#   define IDNAPI
-//#  endif
+#if defined(IDN_DLL)
+#if defined(IDN_DLL_EXPORT)
+#define IDNAPI __declspec(dllexport)
+#else
+#define IDNAPI __declspec(dllimport)
+#endif
+#else
+#define IDNAPI
+#endif
 
